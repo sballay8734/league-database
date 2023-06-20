57a88e7ee99808aaeb026ba5c00f0cd6ca4f2f23
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-06-19 20:06:17 -0400

    Completed themes and mobile nav functionality.

--- src/components/SidebarNav/SidebarNav.js
+++ src/components/SidebarNav/SidebarNav.js
@@ -5,47 +5,54 @@ import { NavigationContext } from "../../context/navigation"
 import { useContext } from "react"
 import PrimaryColorButton from "../PrimaryColorButton"
 import { primaryColors } from "../../theme/themeData"
-import { RxTriangleLeft, RxTriangleRight } from "react-icons/rx"
+import {
+  RxTriangleDown,
+  RxTriangleLeft,
+  RxTriangleRight,
+  RxTriangleUp
+} from "react-icons/rx"
 
 function SidebarNav() {
   const { navIsShown, handleNavClick, handleLinkClick } =
     useContext(NavigationContext)
 
   return (
-    <div className="sidebar-wrapper">
+    <div className={`sidebar-wrapper ${navIsShown ? "show" : ""}`}>
       <div className={`sidebar-nav-links ${navIsShown ? "open" : ""}`}>
         {links.map((link) => {
           return (
             <li
               onClick={() => handleLinkClick(link.path)}
-              className="nav-link"
+              className="sidebar-nav-link"
               key={link.path}
             >
               <Link>{link.label}</Link>
             </li>
           )
         })}
-        <h3 className="theme__header">Customize Theme</h3>
-        <div className="small">Change the theme to your preference</div>
-        <div className="theme__primary-wrapper">
-          <div className="theme__primary-colors">
-            {primaryColors.map((color) => {
-              return (
-                <PrimaryColorButton
-                  onClick={handleNavClick}
-                  key={color.className}
-                  className={color.className}
-                  logo={color.logo}
-                />
-              )
-            })}
+        <div className="sidebar-theme-wrapper">
+          <h3 className="theme__header">Customize Theme</h3>
+          <div className="small">Change the theme to your preference</div>
+          <div className="theme__primary-wrapper">
+            <div className="theme__primary-colors">
+              {primaryColors.map((color) => {
+                return (
+                  <PrimaryColorButton
+                    onClick={handleNavClick}
+                    key={color.className}
+                    className={color.className}
+                    logo={color.logo}
+                  />
+                )
+              })}
+            </div>
           </div>
         </div>
       </div>
       <div onClick={handleNavClick} className="sidebar-nav">
-        <div className="nav-text">NAV</div>
+        <div className="nav-text"></div>
         <div className="nav-arrow">
-          {navIsShown ? <RxTriangleLeft /> : <RxTriangleRight />}
+          {navIsShown ? <RxTriangleUp /> : <RxTriangleDown />}
         </div>
       </div>
     </div>
